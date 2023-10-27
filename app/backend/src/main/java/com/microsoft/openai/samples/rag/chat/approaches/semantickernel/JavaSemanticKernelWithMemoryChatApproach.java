d8d6b14eb558ad2eab87e72be76be24e11ae9695
Milder Hernandez Cagua <milderhc@microsoft.com>
2023-10-27 14:10:45 -0700

    Convert to file plugins

--- app/backend/src/main/java/com/microsoft/openai/samples/rag/chat/approaches/semantickernel/JavaSemanticKernelWithMemoryChatApproach.java
+++ app/backend/src/main/java/com/microsoft/openai/samples/rag/chat/approaches/semantickernel/JavaSemanticKernelWithMemoryChatApproach.java
@@ -58,25 +58,6 @@ public class JavaSemanticKernelWithMemoryChatApproach implements RAGApproach<Cha
 
     @Value("${openai.embedding.deployment}")
     private String embeddingDeploymentModelId;
-
-    private static final String FOLLOW_UP_QUESTIONS_TEMPLATE = """
-    After answering question, also generate three very brief follow-up questions that the user would likely ask next.
-    Use double angle brackets to reference the questions, e.g. <<Are there exclusions for prescriptions?>>.
-    Try not to repeat questions that have already been asked.
-    Only generate questions and do not generate any text before or after the questions, such as 'Next Questions'
-            """;
-    private static final String SYSTEM_CHAT_MESSAGE_TEMPLATE = """
-     Assistant helps the company employees with their healthcare plan questions, and questions about the employee handbook. Be brief in your answers.
-     Answer ONLY with the facts listed in the list of sources below. If there isn't enough information below, say you don't know. Do not generate answers that don't use the sources below. If asking a clarifying question to the user would help, ask the question.
-     For tabular information return it as an html table. Do not return markdown format.
-     Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. Use square brackets to reference the source, e.g. [info1.txt]. Don't combine sources, list each source separately, e.g. [info1.txt][info2.pdf]. 
-     %s     
-    
-     %s
-     Sources:
-     %s
-    """ ;
-
     public JavaSemanticKernelWithMemoryChatApproach(TokenCredential tokenCredential, OpenAIAsyncClient openAIAsyncClient, SearchAsyncClient searchAsyncClient) {
         this.tokenCredential = tokenCredential;
         this.openAIAsyncClient = openAIAsyncClient;
@@ -87,26 +68,30 @@ public class JavaSemanticKernelWithMemoryChatApproach implements RAGApproach<Cha
     public RAGResponse run(ChatGPTConversation questionOrConversation, RAGOptions options) {
         String question = ChatGPTUtils.getLastUserQuestion(questionOrConversation.getMessages());
 
-        //Build semantic kernel with Azure Cognitive Search as memory store. AnswerQuestion skill is imported from resources.
+        // STEP 1: Build semantic kernel with Azure Cognitive Search as memory store. AnswerQuestion skill is imported from resources.
         Kernel semanticKernel = buildSemanticKernel(options);
 
-        List<MemoryQueryResult> sourcesResult = getSourcesFromConversation(questionOrConversation, semanticKernel, options);
+        // STEP 2: Retrieve relevant documents using keywords extracted from the chat history
+        String conversation = ChatGPTUtils.formatAsChatML(questionOrConversation.toOpenAIChatMessages());
+        List<MemoryQueryResult> sourcesResult = getSourcesFromConversation(conversation, semanticKernel, options);
 
         LOGGER.info("Total {} sources found in cognitive vector store for search query[{}]", sourcesResult.size(), question);
 
         String sources = buildSourcesText(sourcesResult);
         List<ContentSource> sourcesList = buildSources(sourcesResult);
 
-        // Use ChatCompletion Service to generate a reply
-        OpenAIChatCompletion chat = (OpenAIChatCompletion) semanticKernel.getService(null, ChatCompletion.class);
-        OpenAIChatHistory history = buildChatHistory(questionOrConversation, options, chat, sources);
+        // STEP 3: Generate a contextual and content specific answer using the search results and chat history
+        SKFunction answerConversation = semanticKernel.getFunction("RAG", "AnswerConversation");
+        SKContext skcontext = SKBuilders.context().build()
+                .setVariable("sources", sources)
+                .setVariable("conversation", conversation)
+                .setVariable("input",  question);
 
-        Mono<String> reply = chat.generateMessageAsync(history, null);
+        Mono<SKContext> reply = answerConversation.invokeAsync(skcontext);
 
         return new RAGResponse.Builder()
-                //.prompt(plan.toPlanString())
                 .prompt("placeholders for prompt")
-                .answer(reply.block())
+                .answer(reply.block().getResult())
                 .sources(sourcesList)
                 .sourcesAsText(sources)
                 .question(question)
@@ -118,36 +103,12 @@ public class JavaSemanticKernelWithMemoryChatApproach implements RAGApproach<Cha
         throw new IllegalStateException("Streaming not supported for this approach");
     }
 
-    private List<MemoryQueryResult> getSourcesFromConversation (ChatGPTConversation conversation, Kernel kernel, RAGOptions options) {
-        String searchQueryPrompt = """
-            Generate a search query for the below conversation.
-            Do not include cited source filenames and document names e.g info.txt or doc.pdf in the search query terms.
-            Do not include any text inside [] or <<>> in the search query terms.
-            Do not enclose the search query in quotes or double quotes.
-            conversation:
-            {{$conversation}}
-            """ ;
+    private List<MemoryQueryResult> getSourcesFromConversation (String conversation, Kernel kernel, RAGOptions options) {
+        SKFunction extractKeywords = kernel.getFunction("RAG", "ExtractKeywords");
+        SKContext skcontext = SKBuilders.context().build().setVariable("conversation", conversation);
 
-        SKContext skcontext = SKBuilders.context().build()
-                .setVariable("conversation", ChatGPTUtils.formatAsChatML(conversation.toOpenAIChatMessages()));
-
-        SKFunction searchQuery = kernel
-                .getSemanticFunctionBuilder()
-                .withPromptTemplate(searchQueryPrompt)
-                .withFunctionName("searchQuery")
-                .withCompletionConfig(
-                        new PromptTemplateConfig.CompletionConfig(
-                                0.2,
-                                1,
-                                0.0,
-                                0.0,
-                                1024
-                        )
-                )
-                .build();
-
-        Mono<SKContext> result = searchQuery.invokeAsync(skcontext);
-        String query = result.block().getResult();
+        Mono<SKContext> result = extractKeywords.invokeAsync(skcontext);
+        String searchQuery = result.block().getResult();
 
         /**
          * Use semantic kernel built-in memory.searchAsync. It uses OpenAI to generate embeddings for the provided question.
@@ -155,32 +116,13 @@ public class JavaSemanticKernelWithMemoryChatApproach implements RAGApproach<Cha
          */
         List<MemoryQueryResult> memoryResult = kernel.getMemory().searchAsync(
                         indexName,
-                        query,
+                        searchQuery,
                         options.getTop(),
                         0.5f,
                         false)
                 .block();
 
         return memoryResult;
-    }
-
-    private OpenAIChatHistory buildChatHistory(ChatGPTConversation conversation, RAGOptions options, OpenAIChatCompletion chat,
-                                               String sources) {
-        String systemMessage =  SYSTEM_CHAT_MESSAGE_TEMPLATE.formatted(
-                options.isSuggestFollowupQuestions() ? FOLLOW_UP_QUESTIONS_TEMPLATE : "",
-                options.getPromptTemplate() != null ? options.getPromptTemplate() : "",
-                sources);
-
-        OpenAIChatHistory chatHistory = chat.createNewChat(systemMessage);
-        conversation.getMessages().forEach(message -> {
-            if(message.role() == ChatGPTMessage.ChatRole.USER){
-                chatHistory.addUserMessage(message.content());
-            } else if(message.role() == ChatGPTMessage.ChatRole.ASSISTANT) {
-                chatHistory.addAssistantMessage(message.content());
-            }
-        });
-
-        return chatHistory;
     }
 
     private List<ContentSource> buildSources(List<MemoryQueryResult> memoryResult) {
@@ -224,7 +166,8 @@ public class JavaSemanticKernelWithMemoryChatApproach implements RAGApproach<Cha
                         .build())
                 .build();
 
-        kernelWithACS.importSkillFromResources("semantickernel/Plugins", "RAG", "AnswerQuestion", null);
+        kernelWithACS.importSkillFromResources("semantickernel/Plugins", "RAG", "AnswerConversation", null);
+        kernelWithACS.importSkillFromResources("semantickernel/Plugins", "RAG", "ExtractKeywords", null);
         return kernelWithACS;
     }
 
