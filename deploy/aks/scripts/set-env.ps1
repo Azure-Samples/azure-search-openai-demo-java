dac3b7bf9086628aa99081c0368e8a012ddc8578
David Minkovski <minkovski.d@gmail.com>
2024-02-02 18:16:03 +0100

    Working on AKS Deployment

---
+++ deploy/aks/scripts/set-env.ps1
@@ -0,0 +1,17 @@
+Write-Host ""
+Write-Host "Loading azd .env file from current environment"
+Write-Host ""
+
+$output = azd env get-values
+
+foreach ($line in $output) {
+  if (!$line.Contains('=')) {
+    continue
+  }
+
+  $name, $value = $line.Split("=")
+  $value = $value -replace '^\"|\"$'
+  [Environment]::SetEnvironmentVariable($name, $value)
+}
+
+Write-Host "Environment variables set."
