106ce7365fec3e5384ab6b9fe318d44fd04404f0
David Minkovski <minkovski.d@gmail.com>
2024-02-23 14:24:42 +0100

    Adjusted file location for powershell

--- deploy/aca/scripts/prepdocs.ps1
+++ deploy/aca/scripts/prepdocs.ps1
@@ -18,7 +18,7 @@ Write-Host "Environment variables set."
 
 
 Write-Host 'Building java indexer..'
-Start-Process -FilePath "mvn"  -ArgumentList "package -f ./app/indexer/pom.xml" -Wait -NoNewWindow
+Start-Process -FilePath "mvn"  -ArgumentList "package -f ../../app/indexer/pom.xml" -Wait -NoNewWindow
 
 Write-Host 'Running the java indexer cli.jar...'
-Start-Process -FilePath "java" -ArgumentList "-jar ./app/indexer/cli/target/cli.jar `"./data`" --storageaccount $env:AZURE_STORAGE_ACCOUNT --container $env:AZURE_STORAGE_CONTAINER --searchservice $env:AZURE_SEARCH_SERVICE --openai-service-name $env:AZURE_OPENAI_SERVICE --openai-emb-deployment $env:AZURE_OPENAI_EMB_DEPLOYMENT --index $env:AZURE_SEARCH_INDEX --formrecognizerservice $env:AZURE_FORMRECOGNIZER_SERVICE --verbose upload" -Wait -NoNewWindow
+Start-Process -FilePath "java" -ArgumentList "-jar ../../app/indexer/cli/target/cli.jar `"../../data`" --storageaccount $env:AZURE_STORAGE_ACCOUNT --container $env:AZURE_STORAGE_CONTAINER --searchservice $env:AZURE_SEARCH_SERVICE --openai-service-name $env:AZURE_OPENAI_SERVICE --openai-emb-deployment $env:AZURE_OPENAI_EMB_DEPLOYMENT --index $env:AZURE_SEARCH_INDEX --formrecognizerservice $env:AZURE_FORMRECOGNIZER_SERVICE --verbose upload" -Wait -NoNewWindow
