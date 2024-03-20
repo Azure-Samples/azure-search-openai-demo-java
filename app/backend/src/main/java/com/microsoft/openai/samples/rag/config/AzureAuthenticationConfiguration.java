4627836e5ade6b4131055dcc635efdaa56baaa81
dantelmomsft <dantelmo@microsoft.com>
2024-03-20 19:05:20 +0100

    java import polish 2

--- app/backend/src/main/java/com/microsoft/openai/samples/rag/config/AzureAuthenticationConfiguration.java
+++ app/backend/src/main/java/com/microsoft/openai/samples/rag/config/AzureAuthenticationConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.util.StringUtils;
 
 @Configuration
 public class AzureAuthenticationConfiguration {
