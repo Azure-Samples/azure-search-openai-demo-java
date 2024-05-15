09d849d52927e785d16e737ea32c07021b468266
dantelmomsft <dantelmo@microsoft.com>
2024-05-15 12:27:51 +0200

    re-enabling cors for better local development/test with react frontend

---
+++ app/backend/src/main/java/com/microsoft/openai/samples/rag/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.microsoft.openai.samples.rag.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
